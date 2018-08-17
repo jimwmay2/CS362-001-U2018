89c176d7c6ed22ebea7acd4377d53ae130846d18
James May <jimwmay2@gmail.com>
2018-08-17 14:55:57 -0700

    assignment 5 setup

---
+++ projects/mayjam/Assignment-5/Calendar/src/test/java/calendar/CalDayRandomTest.java
@@ -0,0 +1,127 @@
+package calendar;
+
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+import java.util.Random;
+
+/**
+ * Random Test Generator  for Appt class.
+ */
+
+public class CalDayRandomTest {
+
+	private static final long TestTimeout = 60 * 500 * 1; /* Timeout at 30 seconds */
+	private static final int NUM_TESTS=100;
+
+	/**
+	 * Return a randomly selected method to be tests !.
+	 */
+    public static String RandomSelectMethod(Random random){
+        String[] methodArray = new String[] {"setTitle","setRecurrence"};// The list of the of methods to be tested in the Appt class
+
+    	int n = random.nextInt(methodArray.length);// get a random number between 0 (inclusive) and  methodArray.length (exclusive)
+    	            
+        return methodArray[n] ; // return the method name 
+        }
+	/**
+	 * Return a randomly selected appointments to recur Weekly,Monthly, or Yearly !.
+	 */
+    public static int RandomSelectRecur(Random random){
+        int[] RecurArray = new int[] {Appt.RECUR_BY_WEEKLY,Appt.RECUR_BY_MONTHLY,Appt.RECUR_BY_YEARLY};// The list of the of setting appointments to recur Weekly,Monthly, or Yearly
+
+    	int n = random.nextInt(RecurArray.length);// get a random number between 0 (inclusive) and  RecurArray.length (exclusive)
+        return RecurArray[n] ; // return the value of the  appointments to recur 
+        }	
+	/**
+	 * Return a randomly selected appointments to recur forever or Never recur  !.
+	 */
+    public static int RandomSelectRecurForEverNever(Random random){
+        int[] RecurArray = new int[] {Appt.RECUR_NUMBER_FOREVER,Appt.RECUR_NUMBER_NEVER};// The list of the of setting appointments to recur RECUR_NUMBER_FOREVER, or RECUR_NUMBER_NEVER
+
+    	int n = random.nextInt(RecurArray.length);// get a random number between 0 (inclusive) and  RecurArray.length (exclusive)
+        return RecurArray[n] ; // return appointments to recur forever or Never recur 
+        }
+
+    /**
+     * Generate Random Tests that tests CalDay Class.
+     */
+	 @Test
+	  public void radnomtest()  throws Throwable  {
+		 long startTime = Calendar.getInstance().getTimeInMillis();
+		 long elapsed = Calendar.getInstance().getTimeInMillis() - startTime;
+		 
+		 Calendar rightnow = Calendar.getInstance();
+		 GregorianCalendar today = new GregorianCalendar(rightnow.get(Calendar.YEAR),rightnow.get(Calendar.MONTH),rightnow.get(Calendar.DAY_OF_MONTH));
+		 CalDay calday1 = new CalDay(today);
+		 
+		 System.out.println("Start testing...");
+		 
+		try{ 
+			for (int iteration = 0; elapsed < TestTimeout; iteration++) {
+				long randomseed =System.currentTimeMillis(); //10
+	//			System.out.println(" Seed:"+randomseed );
+				Random random = new Random(randomseed);
+				 
+				 int startHour=ValuesGenerator.getRandomIntBetween(random, -10, 30);
+				 int startMinute=ValuesGenerator.getRandomIntBetween(random, -30, 90);
+				 int startDay=ValuesGenerator.getRandomIntBetween(random, -10, 45);
+				 int startMonth=ValuesGenerator.getRandomIntBetween(random, -4, 16);
+				 int startYear=ValuesGenerator.getRandomIntBetween(random, -10, 20);
+				 String title="Birthday Party";
+				 String description="This is my birthday party.";
+				 String emailAddress="xyz@gmail.com";
+
+				 //Construct a new Appointment object with the initial data	 
+				 //Construct a new Appointment object with the initial data	 
+		         Appt appt = new Appt(startHour,
+		                  startMinute ,
+		                  startDay ,
+		                  startMonth ,
+		                  startYear ,
+		                  title,
+		                 description,
+		                 emailAddress);
+		         appt.setValid();
+		         calday1.addAppt(appt);
+			 if(!appt.getValid())continue;
+			for (int i = 0; i < NUM_TESTS; i++) {
+					String methodName = CalDayRandomTest.RandomSelectMethod(random);
+					   if (methodName.equals("setTitle")){
+						   String newTitle=(String) ValuesGenerator.getString(random);
+						   appt.setTitle(newTitle);						   
+						}
+					   else if (methodName.equals("setRecurrence")){
+						   int sizeArray=ValuesGenerator.getRandomIntBetween(random, 0, 8);
+						   int[] recurDays=ValuesGenerator.generateRandomArray(random, sizeArray);
+						   int recur=CalDayRandomTest.RandomSelectRecur(random);
+						   int recurIncrement = ValuesGenerator.RandInt(random);
+						   int recurNumber=CalDayRandomTest.RandomSelectRecurForEverNever(random);
+						   appt.setRecurrence(recurDays, recur, recurIncrement, recurNumber);
+						}				
+				}
+				
+				 elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
+			        if((iteration%10000)==0 && iteration!=0 )
+			              System.out.println("elapsed time: "+ elapsed + " of "+TestTimeout);
+			 
+			}
+		}catch(NullPointerException e){
+			
+		}
+	 
+		 System.out.println("Done testing...");
+	 }
+
+		 
+		 
+}
+
+
+	
+
--- projects/mayjam/Assignment-5/README.md
+++ projects/mayjam/Assignment-5/README.md
@@ -1,1 +1,10 @@
-This is my assignment-1 folder 
+Assignment-4. This is NOT part of the team project. Do it on your own!
+
+The goal of this assignment is to write a random test case generator.
+
+
+
+
+
+
+
