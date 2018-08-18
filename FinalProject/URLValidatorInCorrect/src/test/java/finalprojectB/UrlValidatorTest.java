452c6280c30e7c9b00baf39d9f2d493dcde89cb3
James May <jimwmay2@gmail.com>
2018-08-17 19:42:03 -0700

    stuff

--- FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -30,11 +30,33 @@ public class UrlValidatorTest extends TestCase {
    public void testYourFirstPartition()
    {
 	 //You can use this function to implement your First Partition testing	   
+	   
+	   UrlValidator urlVal = new UrlValidator();	// DEFAULT_SCHEMES =  http, https, ftp 
+	   assertFalse(urlVal.isValidScheme("http"));     	// Bug found
+	   assertFalse(urlVal.isValidScheme("https"));		// Bug found
+	   assertFalse(urlVal.isValidScheme("ftp"));      	// Bug found
+	       	
+	   urlVal = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+	   assertTrue(urlVal.isValidScheme("http"));	
+	   assertTrue(urlVal.isValidScheme("https"));  
+	   assertTrue(urlVal.isValidScheme("ftp"));       
+	   assertTrue(urlVal.isValidScheme("file"));	
+	   assertTrue(urlVal.isValidScheme("h2o"));  
+	   assertTrue(urlVal.isValidScheme("FTP"));
+
 
    }
    
    public void testYourSecondPartition(){
 		 //You can use this function to implement your Second Partition testing	   
+	   UrlValidator urlVal = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+	   assertFalse(urlVal.isValid("http://z.com:0"));          	// Bug found
+	   assertFalse(urlVal.isValid("http://z.com:1/"));         	// Bug found
+	   assertFalse(urlVal.isValid("http://z.com:2"));           // Bug found
+	   assertFalse(urlVal.isValid("http://z.com:80/"));         // Bug found  
+	   assertFalse(urlVal.isValid("http://z.com:8080/"));     	// Bug found
+	   assertFalse(urlVal.isValid("http://z.com:21"));          // Bug found
+	   assertFalse(urlVal.isValid("http://1.1.1.1:0"));        	// Bug found
 
    }
    //You need to create more test cases for your Partitions if you need to 
